7fca9d97ece00a9624efbee706f5ea57de037b03
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-03-15 08:21:11 +0600

    countries added

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
 const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
-       .then(res=>res.json())
+        .then(res => res.json())
         .then(data => displayCountries(data))
 }
 const displayCountries = countries => {
-    const countriesHTML = countries.map(country =>getCountryHTML(country));
-    console.log(countriesHTML);
+    const countriesHTML = countries.map(country => getCountryHTML(country));
+    // console.log(countriesHTML[0]);
+    const container = document.getElementById('countries');
+    
+    container.innerHTML = countriesHTML.join(' ');
 
 }
+ 
+
+
 const getCountryHTML = country => {
-    return `<div>
-           <h2>${country.name.common}</h2>
-            </div>     
+    return `
+           <div class="countryCard">
+              <h2>${country.name.common}</h2>
+              <img src="${country.flags.png}" alt="">
+           </div>     
     
     
     `
